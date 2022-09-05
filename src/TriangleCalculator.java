e03d58fbbadb460c56b91da9274ed7e2c0f49e56
SorerBOT <alonfiller@gmail.com>
2022-09-05 16:08:01 +0300

    (Feat) finished calculateArea method

--- src/TriangleCalculator.java
+++ src/TriangleCalculator.java
@@ -2,4 +2,7 @@ public class TriangleCalculator {
     private int base;
     private int height;
 
+    TriangleCalculator(int base, int height) { this.base = base; this.height = height; }
+
+    public static double calculateArea() { return (this.base * this.height) / 2; }
 }
