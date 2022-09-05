b9d84e4bf4d2efe51dba4a104b7b922414c61861
SorerBOT <alonfiller@gmail.com>
2022-09-05 18:57:05 +0300

    (Patch) altered println to print for readability

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,28 @@ public class Main {
         int bronze, silver, gold;
         double base, height;
 
-        System.out.println("Enter amount of 10 points: ");
+        System.out.print("Enter amount of 10 points: ");
         bronze = scanner.nextInt();
 
-        System.out.println("Enter amount of 100 points: ");
+        System.out.print("Enter amount of 100 points: ");
         silver = scanner.nextInt();
 
-        System.out.println("Enter amount of 1000 points: ");
+        System.out.print("Enter amount of 1000 points: ");
         gold = scanner.nextInt();
 
         PointsCounter counter = new PointsCounter(bronze, 10, silver, 100, gold, 1000);
 
         System.out.printf("User has accumulated: %d points.\n", counter.countPoints());
 
-        System.out.println("Enter triangle base: ");
+        System.out.print("Enter triangle base: ");
         base = scanner.nextDouble();
 
-        System.out.println("Enter triangle height: ");
+        System.out.print("Enter triangle height: ");
         height = scanner.nextDouble();
 
         TriangleCalculator triangleCalculator = new TriangleCalculator(base, height);
 
-        System.out.printf("Triangle area is: %f.", triangleCalculator.calculateArea());
+        System.out.printf("Triangle area is: %f.\n", triangleCalculator.calculateArea());
 
 
     }
