fdfe997199d9a885c05df9a4e27fb56acb46e071
SorerBOT <alonfiller@gmail.com>
2022-09-05 16:12:23 +0300

    (Feat) implemented PointsCounter

---
+++ src/PointsCounter.java
@@ -0,0 +1,9 @@
+public class PointsCounter {
+  private int bronze;
+  private int silver;
+  private int gold;
+
+  PointsCounter(int bronze, int silver, int gold) { this.bronze = bronze; this.silver = silver; this.gold = gold; };
+  
+  public int countPoints = { return this.bronze + this.silver + this.gold; }
+}
