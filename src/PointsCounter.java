cf6270473448cd0fe24e3fa07898c531bbdb11ca
SorerBOT <alonfiller@gmail.com>
2022-09-05 16:13:27 +0300

    (Patch) No Java syntax highlighting in neovim ;-;

--- src/PointsCounter.java
+++ src/PointsCounter.java
@@ -5,5 +5,5 @@ public class PointsCounter {
 
   PointsCounter(int bronze, int silver, int gold) { this.bronze = bronze; this.silver = silver; this.gold = gold; };
   
-  public int countPoints = { return this.bronze + this.silver + this.gold; }
+  public int countPoints() { return this.bronze + this.silver + this.gold; }
 }
